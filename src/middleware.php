39dd0582c9e5aec69e8d3dbb6bdca06b488a6b01
Victor Ojeda <ojedavd@hotmail.com>
2018-11-27 16:13:15 -0300

    Inicio

---
+++ src/middleware.php
@@ -0,0 +1,20 @@
+<?php
+// Application middleware
+ 
+// e.g: $app->add(new \Slim\Csrf\Guard);
+$app->add(new \Slim\Middleware\JwtAuthentication([
+    "path" => "/api", /* or ["/api", "/admin"] */
+    "secret" => "supersecretkeyyoushouldnotcommittogithub",
+    "attribute" => "decoded_token_data",
+    "algorithm" => ["HS256"],
+    "callback" => function ($request, $response, $arguments) use ($container) {
+    $container["jwt"] = $arguments["decoded"];
+    },
+    "error" => function ($request, $response, $arguments) {
+    $data["status"] = "error";
+    $data["message"] = $arguments["message"];
+    return $response
+    ->withHeader("Content-Type", "application/json")
+    ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+    }
+    ]));
